32a66ad442e2f533d113358b61b06b0e7425cb3f
santos1920746 <91534472+santos1920746@users.noreply.github.com>
2022-09-11 20:28:25 +0800

    Add files via upload

---
+++ s02/disc/index.js
@@ -0,0 +1,18 @@
+
+    "_id" :("63121f88bab5515d9e662f06"),
+    "FirstName" : "Jack",
+    "LastName" : "Sullivan",
+    "email" : "sullivan@gmail.com"
+
+
+
+    "_id" :("631220d5bab5515d9e662f38"),
+    "FirstName" : "Jack",
+    "LastName" : "Sullivan",
+    "email" : "jsullivan@gmail.com",
+    "credentials" : {
+        "id" : "creds_01",
+        "employee_id" : "2022Dev",
+        "role" : "developer",
+        "team" : "tech"
+    }
